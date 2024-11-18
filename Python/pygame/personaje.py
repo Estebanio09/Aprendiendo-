6e3424539e4f704a69346062e1148a188947a1e0
Esteban Martinez <estebandev09@gmail.com>
2024-11-18 16:20:04 -0500

    Ultimos cambios

--- Python/pygame/personaje.py
+++ Python/pygame/personaje.py
@@ -3,37 +3,49 @@ import pygame
 
 class Personaje:
     def __init__(self, x, y, color, animacion):
-        # se almacena el tiempo en milisegundos desde que inicio pygame
+        # se almacena el tiempo actual en el momento que se creo la instancia de Personaje en milisegundos desde que inicio pygame
         self.update_time = pygame.time.get_ticks()
+        # variable que indica que imagen en la lista se muestra para la animacion 
         self.frame_index = 0
+        # Imagen actual 
         self.imagen = animacion[self.frame_index]
+        # variable para rotar al personaje
         self.flip = False
+        # forma del personaje la cual es un cubo
         self.forma = self.imagen.get_rect()
+        # Posicion del personaje 
         self.forma.center = (x, y)
+        # color de area de colisiones 
         self.color = color
-        self.animaciones = animacion
-        # indice de la animación que se mostrara o la imagen que se mostrara
+        # recibe la lista de las imágenes que componen la animación 
+        self.animaciones = animacion   
         
-        
-        
-        
+    # Método para actualizar el estado constante del personaje 
     def update(self):
+        # tiempo en milisegundos 
         refresco_animación = 100
+        # cambiamos la imagen según cambie el index en de la lista
         self.imagen = self.animaciones[self.frame_index]
+        # se mide el tiempo actual y el tiempo desde el ultimo cambio en (self.update_time // ultima vez que se actualizo) si es mayor a la tasa de refresco 
         if pygame.time.get_ticks() - self.update_time >= refresco_animación:
+            # se suma 1 al index que indica la imagen de la animación
             self.frame_index += 1
+            # se actualiza el tiempo en mi jugador o  (self.update_time // ultima vez que se actualizo)
             self.update_time = pygame.time.get_ticks()
+        # si el index llega a la ultima imagen de las animaciones retorna a 0 y empieza de nuevo 
         if self.frame_index >= len(self.animaciones):
             self.frame_index = 0
             
-
+    # método para realizar el movimiento de mi personaje
     def movimiento(self, delta_x, delta_y):
+        # si el delta x es mayor a 0 flip pasa a true esta variable indica que nuestro personaje giro 
         if delta_x < 0:
             self.flip = True
         elif delta_x > 0:
             self.flip = False
-        self.forma.x = self.forma.x + delta_x
-        self.forma.y = self.forma.y + delta_y
+        self.forma.x += delta_x
+        self.forma.y += delta_y
+        #print(f"{self.forma.x}, {self.forma.y}")
 
     def dibujar(self, interfaz):
         imagen_flip = pygame.transform.flip(self.imagen, self.flip, False)
--- Python/pygame/weapon.py
+++ Python/pygame/weapon.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 
 
@@ -11,10 +12,32 @@ class Weapon:
 
     def update(self, jugador):
         self.forma.center = jugador.forma.center
-        self.forma.x += jugador.forma.width / 2
+        if not jugador.flip:
+            
+            
+            self.forma.x += jugador.forma.width / 2
+            self.rotar_arma(False)
+        if jugador.flip:
+            self.forma.x -= jugador.forma.width / 2
+            self.rotar_arma(True)
         self.forma.y += 10
-        
+
+        # mover la escopeta con el mouse
+        # mouse.ger_pos() trae la posicion del mouse en todas las actualizaciones
+        posicion_mouse = pygame.mouse.get_pos()
+        diferencia_x = posicion_mouse[0] - self.forma.centerx
+        diferencia_y = -(posicion_mouse[1] - self.forma.centery)
+        self.angulo = math.degrees(math.atan2(diferencia_y, diferencia_x))
+
+    def rotar_arma(self, rotar: bool):
+        if rotar:
+            imagen_flip = pygame.transform.flip(self.imagen_original, True, False)
+            self.imagen = pygame.transform.rotate(imagen_flip, self.angulo)
+        else:
+            imagen_flip = pygame.transform.flip(self.imagen_original, False, False)
+            self.imagen = pygame.transform.rotate(imagen_flip, self.angulo)
 
     def dibujar(self, interfaz):
+        self.imagen= pygame.transform.rotate(self.imagen, self.angulo)
         interfaz.blit(self.imagen, self.forma)
-        #pygame.draw.rect(interfaz, self.color, self.forma, width=1)
+        # pygame.draw.rect(interfaz, self.color, self.forma, width=1)
